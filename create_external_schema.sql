1bc42ccdfaaaec3e0f696074b3119f3e8de314e7
Yan-Luo-AU <yanniiluo@gmail.com>
2021-01-26 23:22:51 +1100

    Add files via upload

---
+++ create_external_schema.sql
@@ -0,0 +1,20 @@
+-- method1: create external schema from data catelog
+create external schema dwh_external_data_spectrum from data catalog 
+database 'ecommerce_external_data' 
+iam_role 'YOUR_ARN' 
+region 'eu-west-1';
+
+-- method2: create external schema directly from s3
+create external table dwh_external_data_spectrum.user_data_file(
+	session_id varchar(255),
+	event_timestamp varchar(150),
+	event_type varchar(50),
+	userid varchar(255),
+	product_id varchar(255),
+	category_id varchar(255)
+)
+row format delimited
+fields terminated by ','
+stored as textfile 
+location 's3://BUCKET_NAME/user_behaviour/'
+table properties ('skip.header.line.count'='1')
